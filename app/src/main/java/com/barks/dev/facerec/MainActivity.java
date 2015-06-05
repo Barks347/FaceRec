980a9af630abf545c5811768956bbe27f9ba3420
Barks347 <john.doe92@mail.ru>
2015-06-06 00:41:22 +0300

    Create MainActivity class and layout

---
+++ app/src/main/java/com/barks/dev/facerec/MainActivity.java
@@ -0,0 +1,16 @@
+package com.barks.dev.facerec;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Conserva on 05.06.2015.
+ */
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
